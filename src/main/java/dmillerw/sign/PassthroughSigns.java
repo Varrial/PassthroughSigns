cbfa91b582f1919a3adc55a7afec8949a3bb2bca
GirafiStudios <girafi0@gmail.com>
2015-12-21 21:33:08 +0100

    Updated to 1.8.X Fixed mcmod.info

--- src/main/java/dmillerw/sign/PassthroughSigns.java
+++ src/main/java/dmillerw/sign/PassthroughSigns.java
@@ -1,21 +1,23 @@
 package dmillerw.sign;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.Event;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * @author dmillerw
  */
-@Mod(modid = "PassthroughSigns", name = "PassthroughSigns", version = "%MOD_VERSION%")
+@Mod(modid = "PassthroughSigns", name = "PassthroughSigns", version = "%MOD_VERSION%", acceptedMinecraftVersions = "[1.8,1.9)", dependencies = "required-after:Forge@[11.14.4,)")
 public class PassthroughSigns {
 
     @Mod.Instance("PassthroughSigns")
@@ -29,17 +31,19 @@ public class PassthroughSigns {
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
-            Block block = event.world.getBlock(event.x, event.y, event.z);
-            ForgeDirection forgeDirection = ForgeDirection.getOrientation(event.world.getBlockMetadata(event.x, event.y, event.z)).getOpposite();
+            IBlockState state = event.world.getBlockState(new BlockPos(event.pos.getX(), event.pos.getY(), event.pos.getZ()));
+            Block block = state.getBlock();
+            EnumFacing facing = EnumFacing.getFront(state.getBlock().getMetaFromState(state)).getOpposite();
             if (block == Blocks.wall_sign) {
                 ItemStack held = event.entityPlayer.getHeldItem();
                 if (held != null && held.getItem() instanceof ItemBlock) {
                     event.useItem = Event.Result.DENY;
                 }
                 if (!event.entityPlayer.isSneaking()) {
-                    Block attached = event.world.getBlock(event.x + forgeDirection.offsetX, event.y + forgeDirection.offsetY, event.z + forgeDirection.offsetZ);
-                    if (attached != null && !attached.isAir(event.world, event.x + forgeDirection.offsetX, event.y + forgeDirection.offsetY, event.z + forgeDirection.offsetZ)) {
-                        attached.onBlockActivated(event.world, event.x + forgeDirection.offsetX, event.y + forgeDirection.offsetY, event.z + forgeDirection.offsetZ, event.entityPlayer, event.face, 0, 0, 0);
+                    BlockPos pos = new BlockPos(event.pos.getX() + facing.getFrontOffsetX(), event.pos.getY() + facing.getFrontOffsetY(), event.pos.getZ() + facing.getFrontOffsetZ());
+                    Block attached = event.world.getBlockState(pos).getBlock();
+                    if (attached != null && !attached.isAir(event.world, pos)) {
+                        attached.onBlockActivated(event.world, pos, event.world.getBlockState(pos), event.entityPlayer, event.face, 0, 0, 0);
                     }
                 }
             }
