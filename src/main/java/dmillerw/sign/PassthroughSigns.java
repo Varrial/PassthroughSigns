a6a9d3bb9234c2a01cde89f4832a3f06e2eafa2e
GirafiStudios <girafi0@gmail.com>
2016-04-09 17:01:39 +0200

    Ported to 1.9

--- src/main/java/dmillerw/sign/PassthroughSigns.java
+++ src/main/java/dmillerw/sign/PassthroughSigns.java
@@ -5,8 +5,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,34 +17,29 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 /**
  * @author dmillerw
  */
-@Mod(modid = "PassthroughSigns", name = "PassthroughSigns", version = "%MOD_VERSION%", acceptedMinecraftVersions = "[1.8,1.9)", dependencies = "required-after:Forge@[11.14.4,)")
+@Mod(modid = "PassthroughSigns", name = "PassthroughSigns", version = "%MOD_VERSION%", dependencies = "required-after:Forge@[11.14.4,)")
 public class PassthroughSigns {
-
-    @Mod.Instance("PassthroughSigns")
-    public static PassthroughSigns instance;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(instance);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
-            IBlockState state = event.world.getBlockState(new BlockPos(event.pos.getX(), event.pos.getY(), event.pos.getZ()));
-            Block block = state.getBlock();
-            EnumFacing facing = EnumFacing.getFront(state.getBlock().getMetaFromState(state)).getOpposite();
-            if (block == Blocks.wall_sign) {
-                ItemStack held = event.entityPlayer.getHeldItem();
-                if (held != null && held.getItem() instanceof ItemBlock) {
-                    event.useItem = Event.Result.DENY;
-                }
-                if (!event.entityPlayer.isSneaking()) {
-                    BlockPos pos = new BlockPos(event.pos.getX() + facing.getFrontOffsetX(), event.pos.getY() + facing.getFrontOffsetY(), event.pos.getZ() + facing.getFrontOffsetZ());
-                    Block attached = event.world.getBlockState(pos).getBlock();
-                    if (attached != null && !attached.isAir(event.world, pos)) {
-                        attached.onBlockActivated(event.world, pos, event.world.getBlockState(pos), event.entityPlayer, event.face, 0, 0, 0);
-                    }
+    public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
+        IBlockState state = event.getWorld().getBlockState(event.getPos());
+        Block block = state.getBlock();
+        EnumFacing facing = EnumFacing.getFront(state.getBlock().getMetaFromState(state)).getOpposite();
+        if (block == Blocks.wall_sign) {
+            ItemStack held = event.getEntityPlayer().getHeldItem(event.getHand());
+            if (held != null && held.getItem() instanceof ItemBlock) {
+                event.setUseItem(Event.Result.DENY);
+            }
+            if (!event.getEntityPlayer().isSneaking()) {
+                BlockPos posOffset = new BlockPos(event.getPos().getX() + facing.getFrontOffsetX(), event.getPos().getY() + facing.getFrontOffsetY(), event.getPos().getZ() + facing.getFrontOffsetZ());
+                Block attached = event.getWorld().getBlockState(posOffset).getBlock();
+                if (attached != null && !attached.isAir(state, event.getWorld(), posOffset)) {
+                    attached.onBlockActivated(event.getWorld(), posOffset, event.getWorld().getBlockState(posOffset), event.getEntityPlayer(), event.getHand(), held, event.getFace(), 0, 0, 0);
                 }
             }
         }
